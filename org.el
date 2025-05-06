caaf112f0b86083900687f203b1c9441d20ea989
Kyle Isom <kyle@imap.cc>
2025-05-06 12:54:44 -0700

    fix typo

--- org.el
+++ org.el
@@ -23,7 +23,7 @@
   (setq org-ref-bibliography-files '("~/org/static/references.bib")
 	org-cite-global-bibliography '("~/org/static/references.bib")
         org-ref-default-bibliography "~/org/static/references.bib"
-	org-cite-export-processors '((t csl "~/org/statuc/csl/ieee.csl"))
+	org-cite-export-processors '((t csl "~/org/static/csl/ieee.csl"))
         org-ref-default-citation-style "ieee"
         org-ref-pdf-directory "~/org/static/pdf/"
         org-ref-notes-directory "~/org/"
